59d5054944687f0c30bb8b479cade9f4fb38cd1f
Jamie Pham <jtkp66@gmail.com>
2019-05-01 13:39:45 -0700

    Created front end routes for components to display

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
