b275d35a2d4034fa9a4eee16a8354deea6042249
Jamie Pham <jtkp66@gmail.com>
2019-05-01 17:27:13 -0700

    Added login and logout functionality to Header component, and conditionally render content depending on user's auth status.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return [
+          <li key="11">
+            <a href="/">Admin</a>
+          </li>,
+          <li key="22">
+            <a href="/">Coordinator</a>
+          </li>,
+          <li key="33">
+            <a href="/auth/google">Login</a>
+          </li>
+        ];
+      default:
+        return [
+          <li key="1" />,
+          <li key="3" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.profileName}
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,15 +40,7 @@ class Header extends Component {
             Logo
           </a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="/">Admin</a>
-            </li>
-            <li>
-              <a href="/">Coordinator</a>
-            </li>
-            <li>
-              <a href="/">Login</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -25,4 +48,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
