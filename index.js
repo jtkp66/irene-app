de783bf1ee6c8521079b280bf32b0b8a4f5e11a6
Jamie Pham <jtkp66@gmail.com>
2019-05-01 22:11:27 -0700

    Able to submit form, save to database, and render in dashboard

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ mongoose
   .catch(err => console.log(err));
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(
   cookieSession({
@@ -29,6 +31,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,17 +1,23 @@
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id });
+
+    res.send(surveys);
+  });
+
   app.post("/api/surveys", requireLogin, async (req, res) => {
-    const { title, subject, body } = req.body;
+    const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      // recipients: recipients.split(',').map(email => ({ email }))
       _user: req.user.id,
       dateSent: Date.now()
     });
